f14919dac3b0f703bdce06eede726f2a993bc7c4
BrunoOsio <osiobruno716@gmail.com>
2022-07-23 16:14:06 -0300

    drivetrain sim

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,36 +1,45 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj.motorcontrol.VictorSP;
-import edu.wpi.first.wpilibj.simulation.BatterySim;
 import edu.wpi.first.wpilibj.simulation.DifferentialDrivetrainSim;
-import edu.wpi.first.wpilibj.simulation.RoboRioSim;
+import edu.wpi.first.wpilibj.simulation.DifferentialDrivetrainSim.KitbotGearing;
+import edu.wpi.first.wpilibj.simulation.DifferentialDrivetrainSim.KitbotMotor;
+import edu.wpi.first.wpilibj.simulation.DifferentialDrivetrainSim.KitbotWheelSize;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Robot extends TimedRobot {
+
   private final VictorSP leftMotorFront = new VictorSP(0);
   private final VictorSP leftMotorBack = new VictorSP(1);
   private final VictorSP rightMotorFront = new VictorSP(2);
   private final VictorSP rightMotorBack = new VictorSP(3);
+
   private final MotorControllerGroup leftMotors = new MotorControllerGroup(leftMotorFront, leftMotorBack);
   private final MotorControllerGroup rightMotors = new MotorControllerGroup(rightMotorFront, rightMotorBack);
-  private final DifferentialDrive m_robotDrive = new DifferentialDrive(leftMotors, rightMotors);
+  private final DifferentialDrive drive = new DifferentialDrive(leftMotors, rightMotors);
+
+  private final DifferentialDrivetrainSim driveSim = DifferentialDrivetrainSim.createKitbotSim(
+    KitbotMotor.kDualCIMPerSide, // 2 CIMs per side.
+    KitbotGearing.k10p71,        // 10.71:1
+    KitbotWheelSize.kSixInch,     // 6" diameter wheels.
+    null 
+  );
   private final Joystick m_stick = new Joystick(0);
   private final Timer m_timer = new Timer();
 
-  private Field2d m_field = new Field2d();
+  private Field2d fieldSim = new Field2d();
 
 
   @Override
   public void robotInit() {
     rightMotors.setInverted(true);
-    SmartDashboard.putData("field", m_field);
+    SmartDashboard.putData("field", fieldSim);
   }
 
   @Override
@@ -42,9 +51,9 @@ public class Robot extends TimedRobot {
   @Override
   public void autonomousPeriodic() {
     if (m_timer.get() < 5.0) {
-      m_robotDrive.arcadeDrive(1, 0.3);
+      drive.arcadeDrive(1, 0);
     } else {
-      m_robotDrive.stopMotor();
+      drive.stopMotor();
     }
   }
 
@@ -54,7 +63,7 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopPeriodic() {
-    m_robotDrive.arcadeDrive(m_stick.getY(), m_stick.getX());
+    drive.arcadeDrive(m_stick.getY(), m_stick.getX());
   }
 
   @Override
